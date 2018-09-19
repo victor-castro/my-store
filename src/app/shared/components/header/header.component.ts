670f6d73a3d0c61f3c68bec5051c5365edf1d32b
Fabricio <fabriciobuzatto@gmail.com>
2018-09-19 18:27:10 -0300

    criacao shopping cart

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { map } from 'rxjs/operators';
+import { MatDialog } from '@angular/material';
+import { ShoppingCartComponent } from '../shopping-cart/shopping-cart.component';
 
 @Component({
   selector: 'app-header',
@@ -10,11 +12,31 @@ import { map } from 'rxjs/operators';
 })
 export class HeaderComponent {
 
+  animal: string;
+  name: string;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
     
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public dialog: MatDialog
+  ) {
+
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ShoppingCartComponent, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
 
 }
