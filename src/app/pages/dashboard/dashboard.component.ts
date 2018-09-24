42570855b77c019e8a2dbb7f9709cc8ecad2af8a
Fabricio <fabriciobuzatto@gmail.com>
2018-09-24 18:03:11 -0300

    componente product

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,33 +1,30 @@
 import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'],
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+  products: any[] = [];  
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor() {
+
+    this.products = [
+      {position: 1, name: 'Hydrogen'},
+      {position: 1, name: 'Hydrogen'},
+      {position: 1, name: 'Hydrogen'},
+      {position: 1, name: 'Hydrogen'},
+      {position: 1, name: 'Hydrogen'},
+      {position: 1, name: 'Hydrogen'},
+      {position: 1, name: 'Hydrogen'},
+      {position: 1, name: 'Hydrogen'},
+      {position: 1, name: 'Hydrogen'},
+      {position: 1, name: 'Hydrogen'},
+      {position: 1, name: 'Hydrogen'},
+      {position: 1, name: 'Hydrogen'}
+    ];
+
+  }
 }
